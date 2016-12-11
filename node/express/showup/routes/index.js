e4fd4bb029806ff011ff5ad1ac902bfc9cd0cfaf
Jeff Tchang <jeff.tchang@gmail.com>
2016-12-11 03:26:52 +0000

    Use state for oauth for sending the calendar id

--- node/express/showup/routes/index.js
+++ node/express/showup/routes/index.js
@@ -45,12 +45,14 @@ router.get('/google_auth', function(req, res, next) {
 
 router.get('/google_auth_complete', function(req, res, next) {
 
+	console.log("in google google_auth_complete");
+	
 	var code = req.query.code;
 
 	// This is bad. Storing calid from state
 	var calid = req.query.state;
 
-	console.log(state);
+	console.log(calid);
 
 	oauth2Client.getToken(code, function (err, tokens) {
 
@@ -81,48 +83,13 @@ router.get('/google_auth_complete', function(req, res, next) {
 			    }
 
 					console.log(resp);
-				  res.render('index.html');
+				  res.redirect("/");
 			});
-
-
-			// calendar.events.list({
-			//     auth: oauth2Client,
-			//     calendarId: 'primary',
-			//     timeMin: (new Date()).toISOString(),
-			//     maxResults: 10,
-			//     singleEvents: true,
-			//     orderBy: 'startTime'
-			//   }, function(err, response) {
-			//     if (err) {
-			//       console.log('The API returned an error: ' + err);
-			//       return;
-			//     }
-			//     var events = response.items;
-			//     if (events.length == 0) {
-			//       console.log('No upcoming events found.');
-			//     } else {
-			//       console.log('Upcoming 10 events:');
-			//       for (var i = 0; i < events.length; i++) {
-			//         var event = events[i];
-			//         var start = event.start.dateTime || event.start.date;
-			//         console.log('%s - %s', start, event.summary);
-			//       }
-			//     }
-
-
-			// 		console.log(response);
-			// 	  res.render('index.html');
-
-			//  });
-
-
-
-
-
 
 	  }
 	  else {
 	  	console.log(err);
+	  	res.redirect("/");
 	  }
 
 
